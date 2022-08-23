41e9fcf2c095e70d3edd26fc5a848ce5800e659a
agmorrow <morrow.austin5@gmail.com>
2022-08-23 16:49:18 -0600

    completed tutorial

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,17 @@ const todos_asc = computed(() => todos.value.sort((a, b) => {
   return b.createdAt - a.createdAt
 }))
 
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+watch(todos, newVal => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, { deep: true })
+
+
 const addTodo = () => {
-  if (input_content.value.trim() === ' || input_category.value === null') {
+  if (input_content.value.trim() === '' || input_category.value === null) {
     return
   }
 
@@ -20,18 +29,23 @@ const addTodo = () => {
     content: input_content.value,
     category: input_category.value,
     done: false,
+    editable: false,
     createdAt: new Date().getTime()
   })
-}
-watch(todos, newVal => {
-  localStorage.setItem('todos', JSON.stringify(newVal))
-}, { deep: true })
 
-watch(name, (newVal) => {
-  localStorage.setItem('name', newVal)
-})
+  input_content.value = ''
+  input_category.value = null
+}
+
+
+const removeTodo =todo => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
+
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos')) || []
 })
 </script>
 
@@ -73,7 +87,30 @@ onMounted(() => {
       </form>
     </section>
 
-    
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done &&
+        'done'}`">
+
+          <label>
+            <input type="checkbox" v:model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+
+
+        </div>
+      </div>
+    </section>
+
   </main>
 
 </template>
